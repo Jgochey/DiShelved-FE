4c116278a79322063dc0febc2ccc97e725032cf8
Josh Gochey <josh@gochey.net>
2025-06-02 14:36:34 -0500

    Working CRUD for locationsgit add -A

--- src/api/LocationData.js
+++ src/api/LocationData.js
@@ -1,92 +1,122 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import { clientCredentials } from '../utils/client';
 
 const endpoint = clientCredentials.databaseURL;
 
-const getLocationsByUserUid = async (Uid) => {
-  const response = await fetch(`${endpoint}/Locations/UserUid/${Uid}`, {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-  });
-
-  const data = await response.json();
-  return data || [];
+// Helper to get the current user's token. This will be used to authenticate requests to the API.
+const getToken = async () => {
+  const user = firebase.auth().currentUser;
+  if (user) {
+    return user.getIdToken();
+  }
+  return null;
 };
+
+const getLocationsByUserUid = (Uid) =>
+  new Promise((resolve, reject) => {
+    (async () => {
+      try {
+        const token = await getToken();
+        const response = await fetch(`${endpoint}/Locations/UserUid/${Uid}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = await response.json();
+        resolve(data || []);
+      } catch (error) {
+        reject(error);
+      }
+    })();
+  });
 
 const getLocationById = (locationId) =>
   new Promise((resolve, reject) => {
-    fetch(`${endpoint}/Locations/${locationId}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data) {
-          resolve(data);
-        } else {
-          resolve(null);
-        }
-      })
-      .catch(reject);
+    (async () => {
+      try {
+        const token = await getToken();
+        const response = await fetch(`${endpoint}/Locations/${locationId}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = await response.json();
+        resolve(data || null);
+      } catch (error) {
+        reject(error);
+      }
+    })();
   });
 
 const updateLocation = (locationId, locationData) =>
   new Promise((resolve, reject) => {
-    fetch(`${endpoint}/Locations/${locationId}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(locationData),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data) {
-          resolve(data);
-        } else {
-          resolve(null);
+    (async () => {
+      try {
+        const token = await getToken();
+        const response = await fetch(`${endpoint}/Locations/${locationId}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(locationData),
+        });
+        if (!response.ok) {
+          const errorText = await response.text();
+          reject(new Error(errorText));
+          return;
         }
-      })
-      .catch(reject);
+        const data = await response.json();
+        resolve(data || null);
+      } catch (error) {
+        reject(error);
+      }
+    })();
   });
 
 const deleteLocation = (locationId) =>
   new Promise((resolve, reject) => {
-    fetch(`${endpoint}/Locations/${locationId}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          resolve(true);
-        } else {
-          resolve(false);
-        }
-      })
-      .catch(reject);
+    (async () => {
+      try {
+        const token = await getToken();
+        const response = await fetch(`${endpoint}/Locations/${locationId}`, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        resolve(response.ok);
+      } catch (error) {
+        reject(error);
+      }
+    })();
   });
 
 const createLocation = (locationData) =>
   new Promise((resolve, reject) => {
-    fetch(`${endpoint}/Locations`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(locationData),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data) {
-          resolve(data);
-        } else {
-          resolve(null);
-        }
-      })
-      .catch(reject);
+    (async () => {
+      try {
+        const token = await getToken();
+        const response = await fetch(`${endpoint}/Locations`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(locationData),
+        });
+        const data = await response.json();
+        resolve(data || null);
+      } catch (error) {
+        reject(error);
+      }
+    })();
   });
 
 export { getLocationsByUserUid, getLocationById, updateLocation, deleteLocation, createLocation };
